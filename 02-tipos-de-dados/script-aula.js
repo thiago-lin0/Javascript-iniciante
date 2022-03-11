9200e2b15439b4e837b88303d9587ed026ea8c68
user.name <thiagolinu@gmail,com>
2022-03-10 22:17:25 -0300

    variavei e tipos de dados

---
+++ 02-tipos-de-dados/script-aula.js
@@ -0,0 +1,28 @@
+var idade = 27;
+var simbolo = Symbol();
+console.log(typeof simbolo);
+
+var nome = 'Thiago';
+var sobrenome = 'Lino';
+var nomeCompleto = nome + ' ' + sobrenome; // concatenação
+
+console.log(nomeCompleto);
+
+// var gols = 1000;
+// var frase = 'Romário fez ' + gols + ' gols';
+// console.log(typeof frase);
+
+var ano = '2018';
+var mes = 8;
+console.log(ano + mes); // res 20188
+
+var melhor = 'teste';
+var frase1 = "Esse é o \"melhor\" jogo";
+console.log(frase1);
+
+var gols = 1000;
+var frase2 = `Romário fez ${gols * 2} gols`;  // template string
+
+console.log(frase2);
+
+
---
+++ 02-tipos-de-dados/script.js
@@ -0,0 +1,17 @@
+// Declare uma variável contendo uma string
+
+// Declare uma variável contendo um número dentro de uma string
+
+// Declare uma variável com a sua idade
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas, utilize o template string
+
+// Coloque a seguinte frase em uma variável: It's time
+
+// Verifique o tipo da variável que contém o seu nome
+
+
+
+
+// apagar
