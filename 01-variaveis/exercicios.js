81626171c16a1589ab772ebfa62fe3808480ff50
user.name <thiagolinu@gmail,com>
2022-03-17 10:56:15 -0300

    colocando numeros e operadores

--- 01-variaveis/exercicios.js
+++ 01-variaveis/exercicios.js
@@ -11,3 +11,35 @@
 
 
 // Declarar 5 variáveis diferentes sem valores
+x=3;
+x--;
+if(x<3)
+x--;
+else
+x++;
+x+=2;
+
+console.log(x)
+
+a = 50;
+b = 20;
+c = a +b;
+a = 10;
+
+console.log(a, b, c);
+
+var x1 = 16;
+console.log(true == x1)
+console.log(typeof(42))
+var x = 6%2
+console.log(typeof(x))
+console.log([]==[])
+
+function printA(){
+    let answer;
+    console.log(answer)
+    answer=1;
+}
+
+console.log(printA())
+console.log(printA)
